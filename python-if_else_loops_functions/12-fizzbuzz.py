dc8855aaf43abd970a8c60d78f731c0004bbc11b
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-07 14:00:41 -0700

    fixing pycodestyle and removed unesc fizzbuzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1,101):
+    for i in range(1, 101):
         if i % 15 == 0:
             print("Fizzbuzz", end=" ")
         elif i % 5 == 0:
             print("Buzz", end=" ")
         elif i % 3 == 0:
-            print("Fizz", end= " ")
+            print("Fizz", end=" ")
         else:
             print("{}".format(i), end=" ")
-
-fizzbuzz()
