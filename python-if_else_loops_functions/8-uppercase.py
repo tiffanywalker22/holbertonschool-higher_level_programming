3a9ee84ec76b7d060be0b8bb12f6917e985b927f
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-07 11:47:46 -0700

    adding for loop and print statement

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-def islower(c):
-    return 'A' <= c <= 'Z'
+def uppercase(str):
+    new_str = ""
+    for letter in str
+        if 97 <= ord(c) <= 122:
+            uppercase = char(ord(letter) - 32)
+            new_str += uppercase
+        else:
+            new_str += letter
+    print(new_str)
