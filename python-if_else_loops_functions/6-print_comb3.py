e219cec9e24f4287b2cfce160834d79241229f8e
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-06 17:50:54 -0700

    forgot to include commas

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-for i in range(10):
+for i in range(1, 10):
     for j in range(i + 1, 10):
-        if i != j:
-            if i == 0 and j == 1:
-                print('{}{}'.format(i, j), end='')
-            else:
-                print(', {}{}'.format(i, j), end='')
+        print('{:02d}'.format(i), end=', ')
+        print('{:02d}'.format(j), end=', ')
