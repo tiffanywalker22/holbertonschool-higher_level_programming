5e46aa09f0dc1271ddd1507761ef92eefd11e109
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-07 08:16:13 -0700

    adding negative output

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        print('{}{}'.format(i, j), end=', ')
+        if i != j:
+            if i == 0 and j == 1:
+                print('{}{}'.format(i, j), end='')
+            else:
+                print(', {}{}'.format(i, j), end='')
