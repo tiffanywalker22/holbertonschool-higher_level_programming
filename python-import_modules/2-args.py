68fff96bf84096b491a3928452532c21594980dc
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-07 18:23:46 -0700

    trying one more time to try to get checks

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-import sys
-
-def print_arguments(argv):
-    num_args = len(argv)
-    if num_args == 1:
-        print("1 arguments.")
-        print("1:", argv[0])
-    elif num_args > 1:
-       print(str(num_args) + " arguments:")
-       for i in range(num_args):
-            print(str(i + 1) + ":", argv[i])
-    else:
-        print("0 arguments.")
-        print(".")
 if __name__ == "__main__":
-    print_arguments(sys.argv[1:])
+    import sys
+    num_args = len(sys.argv)
+    if num_args <= 1:
+        print("{} arguments.".format(num_args - 1))
+    elif num_args == 2:
+        print("{} arguments.".format(num_args - 1))
+    else:
+        print("{} arguments.".format(num_args - 1))
+    for x in range(1, num_args):
+        print("{}: {}".format(x, sys.argv[x]))
