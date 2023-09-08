680933b2f0ed39b39925f246b1cc220614014680
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-07 18:37:14 -0700

    fixing typo and spacing issues

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     if num_args == 1:
         print(("0 arguments."))
     else:
-         print("{} {}:".format(args - 1, "argument" if num_args == 2 else "arguments"))
+         print("{} {}:".format(args - 1,
+                                "argument" if num_args == 2 else "arguments"))
     for x in range(1, num_args):
-        print("{}: {}".format(x, sys.argv[x]))
+        print("{}: {}".format(x, argv[x]))
