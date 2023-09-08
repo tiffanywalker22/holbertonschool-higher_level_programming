38bc4fa10187c6c2f2ea78438e618f4213d5d5f7
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-07 17:07:21 -0700

    reformatted args order and added space before arguments to read correctly with checker

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 
 def print_arguments(argv):
     num_args = len(argv)
-    if num_args == 0:
+    if num_args == 1:
+        print("1 arguments.")
+        print("1:", argv[0])
+    elif num_args > 1:
+       print(str(num_args) + " arguments:")
+       for i in range(num_args):
+            print(str(i + 1) + ":", argv[i])
+    else:
         print("0 arguments.")
         print(".")
-    elif num_args == 1:
-        print("1 argument:")
-        print("1:", argv[0])
-    else:
-        print(str(num_args) + "arguments:")
-        position = 1
-        for arg in argv:
-            print(str(position) + ":", arg)
-            position += 1
 if __name__ == "__main__":
+    import sys
     print_arguments(sys.argv[1:])
