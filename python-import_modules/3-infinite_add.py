29f14c5026c12aaf23782d39359bb1c6aac208ff
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-07 19:06:44 -0700

    forgot colon and tabs

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     sum = 0
     if num_args == 1:
         print("0")
-        elif num_args == 2
-            print("{}".format(argv[1]))
-        else:
-            for x in range(1, num_args):
-                sum += int(argv[x])
-            print("{}".format(sum))
+    elif num_args == 2:
+        print("{}".format(argv[1]))
+    else:
+        for x in range(1, num_args):
+            sum += int(argv[x])
+        print("{}".format(sum))
