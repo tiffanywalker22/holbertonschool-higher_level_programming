a741ff053fb5178fb2461a70f192d6000ef5559d
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-08 18:47:46 -0700

    forgot to tab, commit for last checks (hopefully)

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     result = []
     for num in my_list:
         if num % 2 == 0:
-        result.append(True)
-    else:
-        result.append(False)
+            result.append(True)
+        else:
+            result.append(False)
     return (result)
