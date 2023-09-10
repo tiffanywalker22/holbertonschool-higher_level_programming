cfd6da27b15071bf5349730e793d102d67a3e0c4
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-10 16:07:05 -0700

    initial commit

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+
+    for row in matrix:
+        square_row = [x * 2 for x in row]
+        new_matrix.append(square_row)
+    return (new_matrix)
---
+++ python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+learning about sets and dictionaries in python
