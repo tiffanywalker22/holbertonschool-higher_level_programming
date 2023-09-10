e3f0c7cd753655aa57bfea8aca4f72a668fb9915
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-10 16:34:14 -0700

    forgot to test locally, adjusted syntax mistakes

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -4,6 +4,6 @@ def search_replace(my_list, search, replace):
     for item in my_list:
         if item == search:
             new_list.append(replace)
-        else
+        else:
             new_list.append(item)
-        return(new_list)
+        return (new_list)
