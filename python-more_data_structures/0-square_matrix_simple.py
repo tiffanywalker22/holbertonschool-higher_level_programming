0d7ea171f5a89e341cece7c5c063dff70a798bc1
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-10 16:10:36 -0700

    forgot second * to calculate the square correctly

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@ def square_matrix_simple(matrix=[]):
     new_matrix = []
 
     for row in matrix:
-        square_row = [x * 2 for x in row]
+        square_row = [x ** 2 for x in row]
         new_matrix.append(square_row)
     return (new_matrix)
