a5b35405397025408ffa6f1524bdc5c26ad6b0c1
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-10 17:02:15 -0700

    file isnt giving correct output, push to see what checks are missing

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    uniq_int = set()
+    sum = 0
+    for num in my_list:
+        if num not in uniq_int:
+            sum += num
+
+    return (sum)
