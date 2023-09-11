6596b6db383fc5cbccbd64d0fff26c873c89d16b
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-11 12:45:20 -0700

    added print() for newline and added colon to try

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try
+    try:
         count = 0
         for i in range(x):
             print(my_list[i], end="")
             count += 1
+        print()
         return (count)
     except (IndexError, TypeError):
+        print()
         return (count)
