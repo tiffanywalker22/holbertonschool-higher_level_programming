44f8770642be55f4855b7b227034e5dc40a4492f
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-10 19:41:06 -0700

    fixed typo

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        best_key = max(a_dictionary, key=lamdba k: a_dictionary[key])
+        best_key = max(a_dictionary, key=lambda k: a_dictionary[key])
         return (best_key)
     else:
         return (None)
