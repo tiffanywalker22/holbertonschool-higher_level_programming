8cc504d6019e26efc550f7cefa51d9854220c122
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-11 17:22:43 -0700

    needed to fix formatting

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,10 +2,10 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        except (ZeroDivisionError):
-            result = None
-        except  (TypeError, ValueError):
-            result = None
-        finally:
-            print("Inside Result: {}".format(result))
-            return (result)
+    except (ZeroDivisionError):
+        result = None
+    except  (TypeError, ValueError):
+        result = None
+    finally:
+        print("Inside Result: {}".format(result))
+        return (result)
