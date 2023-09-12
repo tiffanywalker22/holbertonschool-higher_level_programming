eda99834e83d1e898c5b32bb8a3d735624eb1a68
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-11 17:27:00 -0700

    file runs locally, but isn't passing checker; trying again

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except (ZeroDivisionError):
+    except ZeroDivisionError:
         result = None
     except  (TypeError, ValueError):
         result = None
