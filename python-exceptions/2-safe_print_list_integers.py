0acf04215ad153b4fdd0820612b634125bf57b81
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-13 08:15:57 -0700

    needed to update print statement to pull item from my list

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        for item in my_list:
-            if isinstance(item, int):
-                print("{:d}".format(item), end= " ")
-                count += 1
-                if count == x:
-                    break
-        print ()
-        return (count)
-    except (TypeError, ValueError):
-        return (count)
+    for item in range(0, x):
+        try:
+            print("{:d}".format(my_list[item])), end="")
+            count += 1
+        except (TypeError, ValueError):
+            continue
+    print()
+    return count
