5455dd9b12a637de915ebf38a35e8f277af20a7c
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-14 18:39:55 -0700

    first commit of task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 0-sqaure.py
+# Tiffany Walker
+"""Defining a class for a Square."""
+
+
+class Square:
+    """The empty class that will define a Square"""
+    
+    def __init__(self, size):
+        """ Initializing the Square"""
+        self.__size = size
