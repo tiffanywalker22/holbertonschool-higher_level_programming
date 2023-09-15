5dc58e27f95b66ce5ab433e18aa95e1cb4f9dd45
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-15 16:35:59 -0700

    updating my_print to handle position

--- python-classes/6-sqaure.py
+++ python-classes/6-sqaure.py
@@ -9,7 +9,7 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """ Initializing the Square"""
-        self.__size = size
+        self.size = size
         self.position = position
 
     @property
@@ -53,5 +53,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
+            for i in range(self.__position[1]):
+                print()
             for i in range(self.__size):
-                print("#" * self.__size)
+                print(" " * self.__position[0] + "#" * self.__size)
