725cb0d9eb2b218e36702e5c2802a1ac8f4029e0
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-15 15:02:45 -0700

    fixed pycodestyle

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -13,14 +13,14 @@ class Square:
 
     def size(self):
         return self.__size
-    
+
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-        
+
     def area(self):
         """ This will print the area of the Square """
         return (self.__size ** 2)
