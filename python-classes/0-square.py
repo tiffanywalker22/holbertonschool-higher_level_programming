2b93d75cf5443abe3cc1bec541d0ed8705d9987a
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-14 18:25:59 -0700

    last try to get documentation to work

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # 0-sqaure.py
 # Tiffany Walker
-""" Defining a class for a Square """
+"""Defining a class for a Square."""
 
 
 class Square:
-    """ The empty class that will define a Square """
+    """The empty class that will define a Square"""
     
     pass
