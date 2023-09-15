4b0b22aa1a778e3a6aca6bf59568f2d11c3c8175
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-15 15:01:19 -0700

    first commit of task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# 4-square.py
+# Tiffany Walker
+"""Defining a class for a Square."""
+
+
+class Square:
+    """The empty class that will define a Square"""
+
+    def __init__(self, size=0):
+        """ Initializing the Square"""
+        self.__size = size
+
+    def size(self):
+        return self.__size
+    
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+        
+    def area(self):
+        """ This will print the area of the Square """
+        return (self.__size ** 2)
