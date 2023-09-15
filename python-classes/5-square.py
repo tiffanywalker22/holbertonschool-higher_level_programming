a51a80e3a5d5d030d8545b0c1fd1ce5e2c9abeda
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-15 15:38:05 -0700

    first commit of task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# 5-square.py
+# Tiffany Walker
+"""Defining a class for a Square."""
+
+
+class Square:
+    """The empty class that will define a Square"""
+
+    def __init__(self, size=0):
+        """ Initializing the Square"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """ Getter method; finds size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Setter method; sets value of size """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ This will print the area of the Square """
+        return (self.__size ** 2)
+def my_print(self):
+    if self.__size == 0:
+        print()
+    else:
+        for i in range(self.__size):
+            print("#" * self.__size)
