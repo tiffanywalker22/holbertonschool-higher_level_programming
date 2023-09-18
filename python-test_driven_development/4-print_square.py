9945e5a357a62f980a8b49666cfdad19e27e62eb
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-18 12:47:16 -0700

    removing typo and fix pycodestyle error

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -7,9 +7,9 @@
 def print_square(size):
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-    
+
     if size < 0:
-        raise ValueError("size must be >= )")
+        raise ValueError("size must be >= 0")
 
     for i in range(size):
         for x in range(size):
