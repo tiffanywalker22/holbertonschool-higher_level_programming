43752a5d81551f6a8113b31a2782ed717f1a41a4
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-18 13:39:43 -0700

    working on test cases

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,6 +5,8 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """ This function should print strings in the format of first_name
+        and last_name"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+# 5-text_indentation.py
+# Tiffany Walker
