711b44f7c0b963ee23812668759a133a3888a954
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-18 14:29:30 -0700

    first commit of code and some tests

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
 # 5-text_indentation.py
 # Tiffany Walker
+""" This function prints text with 2 new lines after each of these 
+    characters ., /, and : """
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    
+    punc_chrs = ['.', '?', ':']
+
+    line = ""
+    for chr in text:
+        line += chr
+
+        if chr in punc_chrs:
+            print(line.strip())
+            line = ""
+    if line:
+        print(line.strip())
