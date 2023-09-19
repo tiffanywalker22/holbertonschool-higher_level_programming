3dc84ca3f6314530f6e7f250efae86679d42ccc1
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-19 16:51:37 -0700

    forgot to include instances in init and class

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -7,10 +7,13 @@
 class Rectangle:
     """ Creating a rectangle with width and height """
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """ Initalize rectangle with width and height """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
