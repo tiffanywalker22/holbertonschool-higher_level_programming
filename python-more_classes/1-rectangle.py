23e4a36746fe9f903ba38432a52c476b57a8f8da
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-19 14:45:42 -0700

    forgot to include decorators; fixed typo; added documentaion

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 0-rectangle.py
+# 1-rectangle.py
 # Tiffany Walker
 """ Creating an empty class for Rectangle  """
 
@@ -12,23 +12,30 @@ def __init__(self, width=0, height=0):
     self.width = width
     self.height = height
 
+@property
 def width(self):
+    """ Retrieves width -- setter method """
     return self.__width
 
+@width.setter
 def width(self, value):
+    """ Sets width -- getter method """
     if not isinstance(value, int):
         raise TypeError("width must be an integer")
     if value < 0:
         raise ValueError("width must be >= 0")
     self.__width = value
 
+    @property
     def height(self):
+        """Retrieves height -- getter method """
         return self.__height
     
+    @height.setter
     def height(self, value):
+        """ Sets height - setter method """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-        
