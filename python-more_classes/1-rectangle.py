d1e49a8b147444b3140d83e5a8a46f8bb03fd115
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-19 14:52:11 -0700

    fixed indents

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,30 +7,35 @@
 class Rectangle:
     """ Creating a rectangle with width and height """
 
-def __init__(self, width=0, height=0):
-    """ Initalize rectangle with width and height """
-    self.width = width
-    self.height = height
 
-@property
-def width(self):
-    """ Retrieves width -- setter method """
-    return self.__width
+    def __init__(self, width=0, height=0):
+        """ Initalize rectangle with width and height """
+        self.width = width
+        self.height = height
 
-@width.setter
-def width(self, value):
-    """ Sets width -- getter method """
-    if not isinstance(value, int):
-        raise TypeError("width must be an integer")
-    if value < 0:
-        raise ValueError("width must be >= 0")
-    self.__width = value
+
+    @property
+    def width(self):
+        """ Retrieves width -- setter method """
+        return self.__width
+
+
+    @width.setter
+    def width(self, value):
+        """ Sets width -- getter method """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
 
     @property
     def height(self):
         """Retrieves height -- getter method """
         return self.__height
-    
+
+
     @height.setter
     def height(self, value):
         """ Sets height - setter method """
