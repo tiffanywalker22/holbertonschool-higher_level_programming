ce6ab6a48b4962619966d06568d2af8766536490
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-20 09:11:25 -0700

    adding more tests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -12,4 +12,11 @@ class TestMaxInteger(unittest.Testcase)
 
 def test_max_integer(self):
     self.assertEqual(max_integer([2]), 2)
+    self.assertEqual(max_integer([-2]), -2)
+    self.assertEqual(max_integer([]), 2)
 
+def test_mixed_integer(self):
+    self.assertEqual(max_integer(-1, 2, -3, 4) -5)
+
+def test_large_list(self):
+    self.assertEqual(max_integer(list(range(1, 10001))), 10000)
