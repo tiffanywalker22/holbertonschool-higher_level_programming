7ff53b69d56942a59d32ebb54bb1efe803208e57
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-20 09:54:29 -0700

    adding updates to tests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
 # 6-max_integer_test.py
 # Tiffany Walker
-    """ Unitest for max_integer """
+""" Unitest for max_integer """
+
 
 import unittest
 
 max_integer = __import__('6-max_integer').max_integer
 
+class TestMaxInteger(unittest.TestCase):
+    """ Class for unittest """
 
-class TestMaxInteger(unittest.Testcase)
+    def test_max_integer(self):
+        self.assertEqual(max_integer([2]), 2)
+        self.assertEqual(max_integer([-2]), -2)
+        self.assertNotEqual(max_integer([]), 2)
+        self.assertNotEqual(max_integer([2]), -2)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([5, 5, 5, 5]), 5)
+        self.assertEqual(max_integer([1, 4, 7, 8]), 8)
 
-def test_max_integer(self):
-    self.assertEqual(max_integer([2]), 2)
-    self.assertEqual(max_integer([-2]), -2)
-    self.assertEqual(max_integer([]), 2)
-    self.assertEqual(max_integer([2]), -2)
-    self.assertEqual(max_integer([]), None)
-    self.assertEqual(max_integer([5, 5, 5, 5]) 5)
-    self.assertEqual(max_integer([1, 4, 7, 8]) 9)
+    def test_mixed_integer(self):
+        self.assertEqual(max_integer([-1, 2, -3, -5]), 2)
+        self.assertEqual(max_integer([-2, 3, -4, -6]), 3)
 
-def test_mixed_integer(self):
-    self.assertEqual(max_integer([-1, 2, -3, 4]) -5)
-    self.assertEqual(max_integer([-2, 3, -4, 5]) -6)
-
-def test_large_list(self):
-    self.assertEqual(max_integer(list(range(1, 10001))), 10000)
+    def test_large_list(self):
+        self.assertEqual(max_integer(list(range(1, 10001))), 10000)
 
 if __name__ == '__main__':
     unittest.main()
