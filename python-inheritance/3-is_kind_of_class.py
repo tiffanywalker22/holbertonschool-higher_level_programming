02437a5416a8a4a122ab5962f7d0f779db8e1a2f
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-22 15:27:09 -0700

    fix pycode; needed to add space and an extra line

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-#3-is_kind_of_clas.py
+# 3-is_kind_of_clas.py
 # Tiffany Walker
 """ Function that determines if object is inherited, or an instance """
+
 
 def is_kind_of_class(obj, a_class):
     """ Checks if obj is an instance or inherited by the class """
