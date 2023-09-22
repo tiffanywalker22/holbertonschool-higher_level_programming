294b5afee1bd2b398310a0738d4a1ee2f5bce07b
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-22 13:53:15 -0700

    first commit

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 0-lookup.py
+# Tiffany Walker
+""" Function that returns list of attributes and
+methods of an object"""
+
+
+def lookup(obj):
+
+    return dir(obj)
---
+++ python-inheritance/README.md
@@ -0,0 +1,1 @@
+Learning about inheritance in python
