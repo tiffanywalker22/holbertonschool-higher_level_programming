6f7eff5e08ee6171b210b2e4579cd9328d8d6349
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-22 16:27:07 -0700

    adding task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+# Tiffany Walker
+""" The function creates an empty class """
+
+
+class BaseGeometry:
+    """ The empty class """
+    
+    def area(self):
+        raise Exception("area() is not implemented")
