9e83690a27c5b3bb9d1f4fbbc254fc7fe281c4aa
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-22 18:17:25 -0700

    getting 7 set up; adding code

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# 7-base_geometry.py
+# Tiffany Walker
+""" The function creates an empty class """
+
+
+class BaseGeometry:
+    """ The empty class """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """ Checks the int value """
+        if not isinstance(value, int):
+            raise TypeError(f"<name> must be an integer")
+        if value <= 0:
+            raise ValueError(f"<name> must be greater than 0")
+            
