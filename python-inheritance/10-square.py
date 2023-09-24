032faea260258bc1f506828edc9e28cad60104ec
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-24 14:50:40 -0700

    file runs locally; pushing to checker

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 10-square.py
+# Tiffany Walker
+""" Class basegeometry, subclass rectangle, subclass square """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Class for Square """
+
+    def __init__(self, size):
+        """ Initialize square with size """
+        super().integer_validator("size", size)
+        self.__size = size 
+        super().__init__(size, size)
