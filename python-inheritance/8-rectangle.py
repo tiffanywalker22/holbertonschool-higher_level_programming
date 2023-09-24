5cfd994633c7637e3c68cbf30f02a47d81ad921e
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-24 13:56:32 -0700

    8 still not running locally; pushing to see checker

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,15 +4,12 @@
 """ Class Rectanlge using BaseGeometry """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """ The class Rectangle that is inheriting BaseGeometry """
-    def __init__(self, width, height):
-        self.__width = 0
-        self.__height = 0
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
 
-    def area(self):
-        return self.__width * self.__height
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
