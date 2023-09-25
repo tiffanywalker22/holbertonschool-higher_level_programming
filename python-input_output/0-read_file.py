2676f8ccaa9bafc5fd7ab419961eeae6330a0414
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-24 18:37:25 -0700

    first commit

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 0-read_file
+# Tiffany Walker
+""" Function that reads a text file """
+
+
+def read_file(filename=""):
+    with open(filename, encoding="etf-8") as file:
+        for line in file:
+            print(line, end="")
