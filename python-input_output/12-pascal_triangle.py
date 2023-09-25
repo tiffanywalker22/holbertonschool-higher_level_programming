44d1fcce0b93ad234a730c960510df7c36af5257
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-25 14:42:08 -0700

    needed to account for element abouve current element

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -15,7 +15,7 @@ def pascal_triangle(n):
         new_row = [1]
 
         for x in range(1, i):
-            new_row.append(prev_row[x - 1])
+            new_row.append(prev_row[x - 1] + prev_row[x])
 
         new_row.append(1)
         triangle.append(new_row)
