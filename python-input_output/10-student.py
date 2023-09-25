3f9dbdec5a09c8a70107be541f0d4cca12f2f915
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-25 13:46:09 -0700

    adding documentation and first commit of 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# 10-student.py
+# Tiffany Walker
+""" function creates class student """
+
+
+class Student:
+    """ Class for Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """ initialize first name, last name, age """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            result = {
+                attr: getattr(self, attr)
+                for attr in attrs if hasattr(self, attr)
+            }
+            return result
--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,6 +10,7 @@ load = __import__('6-load_from_json_file').load_from_json_file
 save = __import__('5-save_to_json_file').save_to_json_file
 
 def add_items_to_json():
+    """ add existing items from file or create empty list """
     try:
         items = load('add_item.json')
     except FileNotFoundError:
