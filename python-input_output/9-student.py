ffd990fac2c3a15d9290a97d912fd70ab3306365
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-25 13:13:08 -0700

    fix pycode and typo

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -10,8 +10,8 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """ initialize first name, last name, age """
         self.first_name = first_name
-        self.ladt_name - last_name
+        self.last_name - last_name
         self.age = age
-    
+
     def to_json(self):
         return self.__dict__
