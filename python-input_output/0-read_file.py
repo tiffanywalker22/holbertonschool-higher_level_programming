ea98d6c4c86409101158b7ed5bde507b5dbd5353
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-24 18:40:47 -0700

    fix typo

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 
 
 def read_file(filename=""):
-    with open(filename, encoding="etf-8") as file:
+    with open(filename, encoding="utf-8") as file:
         for line in file:
             print(line, end="")
