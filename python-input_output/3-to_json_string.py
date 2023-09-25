24565ec0a2a67ef8a1c57f4d215d71b4e0ca7867
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-24 19:28:45 -0700

    adding json

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 3-to_json_string.py
+# Tiffany Walker
+""" Function that returns the JSON representation of an object """
+import json
+
+def to_json_string(my_obj):
+    """ Return JSON representation of obj """
+    return json.dumps(my_obj)
