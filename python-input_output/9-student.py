4eb9dc8442de3ace8803e3aeff82a9532f15de95
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-25 13:08:43 -0700

    adding student

---
+++ python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 9-student.py
+# Tiffany Walker
+""" function creates class student """
+
+
+class Student:
+    """ Class for Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """ initialize first name, last name, age """
+        self.first_name = first_name
+        self.ladt_name - last_name
+        self.age = age
+    
+    def to_json(self):
+        return self.__dict__
