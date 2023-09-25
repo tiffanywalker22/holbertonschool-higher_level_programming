5debc2b9903260a20f74bc79a1ae11833f4943b3
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-24 19:17:37 -0700

    fixed pycode adn added documentation

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,6 +5,8 @@
 
 
 def append_write(filename="", text=""):
+    """ Function appends a string at end of text file and returns
+    number of characters added """
     with open(filename, mode="a", encoding="utf-8") as file:
         chr_cnt = file.write(text)
-    return chr_cnt 
+    return chr_cnt
