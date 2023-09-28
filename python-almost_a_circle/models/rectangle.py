bf24e6e9a36190e53aab750c1b4345ad1c96b533
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-28 16:31:41 -0700

    adding documentation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,9 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """ class for rectangle """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ initialize width, height, x, and y """
         super().__init__(id)
         self.width = width
         self.height = height
@@ -17,40 +19,48 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """ returns width """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ setter method for width of rectangle """
         if value <= 0:
             raise ValueError("Width must be greater than 0")
         self.__width = value
 
     @property
     def height(self):
+        """ returns height """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ setter method for height of rectangle """
         if value <= 0:
             raise ValueError("Height must be greater than 0")
         self.__height = value
 
     @property
     def x(self):
+        """ returns x coordinate"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """ setter method for x"""
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
+        """ returns y coordinate """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """ setter method for y """
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
