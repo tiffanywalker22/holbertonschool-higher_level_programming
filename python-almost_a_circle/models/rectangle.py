c703170ddeed76d43b9315bf2d129198e5b3cbe3
tiffanywalker22 <tbcasper16@gmail.com>
2023-09-28 16:25:58 -0700

    fixed pycodestyle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,10 +5,12 @@
 
 
 from models.base import Base
+
+
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.width = width 
+        self.width = width
         self.height = height
         self.x = x
         self.y = y
@@ -16,7 +18,7 @@ class Rectangle(Base):
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if value <= 0:
@@ -26,17 +28,17 @@ class Rectangle(Base):
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if value <= 0:
             raise ValueError("Height must be greater than 0")
         self.__height = value
-    
+
     @property
     def x(self):
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         if value < 0:
@@ -46,9 +48,9 @@ class Rectangle(Base):
     @property
     def y(self):
         return self.__y
-    
+
     @y.setter
-    def y(self,value):
+    def y(self, value):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
