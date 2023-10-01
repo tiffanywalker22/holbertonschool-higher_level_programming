2b32953e2289855132f09eab0d426f7149c2072f
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-01 15:07:07 -0700

    adding getter/setter

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -17,3 +17,12 @@ class Square(Rectangle):
     def __str__(self):
         """ returns string rep of square """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+    
+    @property
+    def size(self):
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value 
