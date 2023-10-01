dd82fa647fbcbbf2e357a15e91d66ac63e04e289
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-01 14:12:44 -0700

    adding kwargs

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -89,16 +89,27 @@ class Rectangle(Base):
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - " \
             f"{self.__width}/{self.__height}"
 
-    def update(self, *args):
-        """ assigns args to each attribute """
-        arg_count = len(args)
-        if arg_count > 0:
-            self.id = args[0]
-        if arg_count > 1:
-            self.width = args[1]
-        if arg_count > 2:
-            self.height = args[2]
-        if arg_count > 3:
-            self.x = args[3]
-        if arg_count > 4:
-            self.y = args[4]
+    def update(self, *args, **kwargs):
+        """ assigns a key_value argument to attributes """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+        else:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'width' in kwargs:
+                self.width = kwargs['width']
+            if 'height' in kwargs:
+                self.height = kwargs['height']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']            
