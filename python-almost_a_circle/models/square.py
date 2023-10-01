f02a83f1f4652514e727057cab7d9b173150ec98
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-01 16:09:24 -0700

    addig dict to square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -49,3 +49,11 @@ class Square(Rectangle):
                 self.x = kwargs['x']
             if 'y' in kwargs:
                 self.y = kwargs['y']
+    def to_dictionary(self):
+        """ returns dictionary rep of square """
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+            }
