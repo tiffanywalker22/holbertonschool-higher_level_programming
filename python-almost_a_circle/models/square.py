beaec842af10eac6a72685ca9fff14e35459d951
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-01 15:44:40 -0700

    add documentation

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -30,6 +30,7 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
+        """ assigns attrubutes to square """
         if args:
             if len(args) >= 1:
                 self.id = args[0]
