c990bfd1084d3ad6d4efdcd99e85c020796ecd2b
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-01 16:22:40 -0700

    adding json

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,6 +4,8 @@
 """ Our base class for all other clases """
 
 
+import json
+
 class Base:
     """ Our class 'base' """
     __nb_objects = 0
@@ -14,3 +16,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ return json rep of list_dictionaries """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
