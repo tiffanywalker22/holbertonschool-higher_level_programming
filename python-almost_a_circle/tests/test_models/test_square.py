25d5ed55b98ac5acd1a59990bbce9bb99caa226b
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-03 20:16:02 -0700

    adding more tests

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -13,3 +13,16 @@ class TestSquare(unittest.TestCase):
         """ testing no arguments """
         with self.assertRaises(TypeError):
             Square()
+
+    def test_values(self):
+        """ tests valid values """
+        square = Square(2, 4, 6, 1)
+        self.assertEqual(square.id, 1)
+        self.assertEqual(square.size, 2)
+        self.assertEqual(square.x, 4)
+        self.assertEqual(square.y, 6)
+
+    def test_invalid_size(self):
+        """ test for invalid size """
+        with self.assertRaises(TypeError):
+            square = Square("invalid", 20, 30, 40, 1)
