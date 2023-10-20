f46adab0fe3a9765bc60f3fab59ac645bcf78bd3
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-20 15:13:02 -0700

    had o instead of 0

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- script that creates database hbtn_od_2 and user user_od_2
-CREATE DATABASE IF NOT EXISTS hbtn_od_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES ON hbtn_od_2* to 'user_0d_2'@'localhost';
+GRANT ALL PRIVILEGES ON hbtn_0d_2* to 'user_0d_2'@'localhost';
