f083212cb58313f30eb56365cb7709111355e56b
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-23 15:30:50 -0700

    fixing mistake on task 2; needed to use select

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- script that creates database hbtn_0d_2 and user user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES ON hbtn_0d_2.* to 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* to 'user_0d_2'@'localhost';
