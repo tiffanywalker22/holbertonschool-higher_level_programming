7969aea746fa20416d6bdc2d1e0fb2d2a2cf77fc
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-23 14:37:18 -0700

    my genres

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
 -- script lists all genres contained in hbtn_0d_tvshows and displays the number of shows linked to each
-SELECT tv_genres.name AS genre,
-COUNT(show_id) AS number_of_shows
-FROM tv_genres
-JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+SELECT tv_genres.name
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
