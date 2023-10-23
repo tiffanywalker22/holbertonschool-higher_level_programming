9e30e210571b5c284ac6f30a8885a20b9af05b0f
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-23 14:45:14 -0700

    figured out mistake; needed single quotes instead of doubles, also fixed desc

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
--- script lists all genres contained in hbtn_0d_tvshows and displays the number of shows linked to each
+-- script that uses hbtn_0d_tvshows database to list all genres of the show Dexter
 SELECT tv_genres.name
 FROM tv_shows
 JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id
 JOIN tv_genres ON tv_shows_genres.genre_id = tv_genres.id
-WHERE tv_shows.title = "Dexter"
+WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name;
