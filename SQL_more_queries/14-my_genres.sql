a47ef75c6d734a9aa4f518cd768ab9e4cb8a671b
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-23 15:08:29 -0700

    final fix on task 14; adding only comedy

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- script that uses hbtn_0d_tvshows database to list all genres of the show Dexter
 SELECT tv_genres.name
 FROM tv_shows
-JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id
-JOIN tv_genres ON tv_shows_genres.genre_id = tv_genres.id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name;
