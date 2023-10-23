56ce6bdc6fa467eaf9c5409e8570a08c67d52efa
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-23 16:34:38 -0700

    fist commit; getting set up

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Script that lists all states from database hbtn_0e_usa"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         password=sys.arv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states')
+    states = cursor.fetchall()
+    
+    for state in states:
+        print(state)
---
+++ python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+Learning about object relational mapping
