cef561fa8ff42aa8ed98632ac16e992a4d3a14f7
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-24 16:22:31 -0700

    fixed connection string

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,8 +6,8 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine(f'mysql+mysql://{sys.argv[1]}:{sys.argv[2]}
-                           @localhost:3306/{sys.argv[3]}',pool_pre_ping=True)
+    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}'
+                          f'@localhost:3306/{sys.argv[3]}',pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
