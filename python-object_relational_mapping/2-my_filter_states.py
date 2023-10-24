61dc657a46fc458cdeeb8e038b54479c681f2032
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-23 17:54:06 -0700

    fix line over 80chars

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          password=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = '{}'".format(sys.argv[4]))
+    cursor.execute("SELECT * FROM states WHERE name = '{}'"
+                   .format(sys.argv[4]))
     states = cursor.fetchall()
 
     for state in states:
