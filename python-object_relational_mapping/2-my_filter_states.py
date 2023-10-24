e6a1ac5cbb8e3d80d4798e6857b66eaa265cf575
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-23 17:48:36 -0700

    file is running locally; pushing to checker

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" script takes in argument and displays all values in states table of
+hbtn_0e_0_usa where name matches argument """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         password=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = '{}'".format(sys.argv[4]))
+    states = cursor.fetchall()
+
+    for state in states:
+        if state[1] == sys.argv[4]:
+            print(state)
