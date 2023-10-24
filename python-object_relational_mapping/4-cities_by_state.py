ff076fd34b5015add2090177619810630298f70d
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-23 19:08:38 -0700

    fixed cities.state_id

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          password=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("""SELECT cities.id, cities.name, states.name
-                   FROM cities JOIN states ON cities.state.id = states.id
+                   FROM cities JOIN states ON cities.state_id = states.id
                    ORDER BY cities.id;
                    """)
     cities = cursor.fetchall()
