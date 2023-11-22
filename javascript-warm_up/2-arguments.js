f7b67671c05b0c85cbe0e2ef01047f4600fa7e05
tiffanywalker22 <tbcasper16@gmail.com>
2023-11-22 10:12:21 -0800

    adding task 2

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (process.argv.length === 2) {
+console.log('No argument');
+}
+else if (process.argv.length === 1) {
+console.log('Argument Found')
+}
+else {
+    console.log('Arguments found')
+}
