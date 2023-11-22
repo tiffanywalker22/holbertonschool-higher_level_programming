818a9a7662f6c6044c18a03a744f02188dd638cb
tiffanywalker22 <tbcasper16@gmail.com>
2023-11-22 11:45:26 -0800

    first try of 5

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const args = process.argv;
+const ints = parseInt(args[2]);
+
+if (isNaN(ints)) {
+    console.log('Not a number');
+}
+else {
+    console.log(`My number: ${ints}`);
+}
+
