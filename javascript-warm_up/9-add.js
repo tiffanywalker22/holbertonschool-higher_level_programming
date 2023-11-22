7000eb12363aa50ab86f12eed756987cad94e71b
tiffanywalker22 <tbcasper16@gmail.com>
2023-11-22 12:22:52 -0800

    adding task 9

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv
+const num1 = parseInt(args[2]);
+const num2 = parseInt(args[3]);
+
+if (isNaN(num2)) {
+    console.log(`NaN`);
+  } else {
+    console.log(num1 + num2);
+  }
