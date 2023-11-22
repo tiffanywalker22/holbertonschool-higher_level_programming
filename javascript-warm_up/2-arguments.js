e5f17bbf49de9c8381d41f02f5ca36d8eed213c8
tiffanywalker22 <tbcasper16@gmail.com>
2023-11-22 10:23:31 -0800

    file would not push to github; delete and remake; needed to fix args

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 if (process.argv.length === 2) {
-console.log('No argument');
-}
-else if (process.argv.length === 3) {
-console.log('Argument Found');
-}
-else {
-    console.log('Arguments found');
-}
+    console.log('No argument');
+    }
+    else if (process.argv.length === 3) {
+    console.log('Argument Found');
+    }
+    else {
+        console.log('Arguments found');
+    }
