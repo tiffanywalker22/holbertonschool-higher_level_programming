312f3e03543c85f3ea3344134a40e022407debdf
tiffanywalker22 <tbcasper16@gmail.com>
2023-11-22 10:15:37 -0800

    needed to correct args

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 if (process.argv.length === 2) {
 console.log('No argument');
 }
-else if (process.argv.length === 1) {
-console.log('Argument Found')
+else if (process.argv.length === 3) {
+console.log('Argument Found');
 }
 else {
-    console.log('Arguments found')
+    console.log('Arguments found');
 }
