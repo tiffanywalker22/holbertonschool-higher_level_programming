914b94e8c84573e5535a1289f310997a0d647a32
tiffanywalker22 <tbcasper16@gmail.com>
2023-11-25 15:32:49 -0800

    semistand fix

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 function factorial (x) {
-    if (isNaN(x)) {
-        return 1;
-    } else if (x === 0) {
-        return 1;
-    } else {
-        return x * factorial(x - 1);
-    }
+  if (isNaN(x)) {
+    return 1;
+  } else if (x === 0) {
+    return 1;
+  } else {
+    return x * factorial(x - 1);
+  }
 }
-const x = parseInt(process.argv[2])
-console.log(factorial(x))
+const x = parseInt(process.argv[2]);
+console.log(factorial(x));
