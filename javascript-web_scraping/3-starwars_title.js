d1f672681d2f336d0d36ce002548458a170bc598
tiffanywalker22 <tbcasper16@gmail.com>
2023-12-01 16:46:54 -0800

    adding starwars

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+const movie = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
