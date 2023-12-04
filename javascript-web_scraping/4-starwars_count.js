af57b6d2c5eb1123114087a2455b7d1df4350cef
tiffanywalker22 <tbcasper16@gmail.com>
2023-12-04 14:49:35 -0800

    revisiting task 4 now that i've completed other tasks; pushing to view checker

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const request = require('request');
+const charId = 18;
+const url = 'https://swapi-api.hbtn.io/api/films/';
+
+request.get(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const films = JSON.parse(body).results;
+    const wedgemovies = films.filter(film =>
+      film.characters.includes(`https://swapi-api.hbtn.io/api/people/${charId}/`)
+    );
+    console.log(wedgemovies.length);
+  }
+});
