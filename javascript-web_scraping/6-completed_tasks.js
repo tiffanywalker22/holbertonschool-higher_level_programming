912dc294f314ce90bfc2ea4097159632447e315b
tiffanywalker22 <tbcasper16@gmail.com>
2023-12-04 14:20:45 -0800

    trying to fix body is not defined error; code is now accessing body correctly... i thhink

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -6,12 +6,13 @@ const taskscomp = {};
 request(url, function (error, response, body) {
   if (error) {
     console.error(error);
+  } else {
+    const tasks = JSON.parse(body);
+    tasks.forEach((task) => {
+      if (task.completed) {
+        taskscomp[task.userId] = (taskscomp[task.userId] || 0) + 1;
+      }
+    });
+    console.log(taskscomp);
   }
 });
-const tasks = JSON.parse(body);
-tasks.forEach((task) => {
-  if (task.completed) {
-    taskscomp[task.userId] = (taskscomp[task.userId] || 0) + 1;
-  }
-});
-console.log(taskscomp);
