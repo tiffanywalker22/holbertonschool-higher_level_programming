12257a248ad9a0e3e902252a94d076c591fab239
tiffanywalker22 <tbcasper16@gmail.com>
2023-12-04 14:11:20 -0800

    pushing task 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+const taskscomp = {};
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+});
+const tasks = JSON.parse(body);
+tasks.forEach((task) => {
+  if (task.completed) {
+    taskscomp[task.userId] = (taskscomp[task.userId] || 0) + 1;
+  }
+});
+console.log(taskscomp);
