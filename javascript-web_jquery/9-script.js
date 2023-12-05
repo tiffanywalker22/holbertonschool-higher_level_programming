c0218f3bcf8c84a9ad14ea29ef50320285045674
tiffanywalker22 <tbcasper16@gmail.com>
2023-12-04 17:29:08 -0800

    adding final task

---
+++ javascript-web_jquery/9-script.js
@@ -0,0 +1,5 @@
+const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+$.get(url, function (responsedata) {
+  const translatehello = responsedata.hello;
+  $('hello').text(translatehello);
+});
